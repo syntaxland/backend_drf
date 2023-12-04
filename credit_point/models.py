94c2afae992127970ece79366b1c8676607dc6d0
syntaxland <chibuzo.okenwa@email.com>
2023-12-04 21:45:42 +0100

    new update

--- credit_point/models.py
+++ credit_point/models.py
@@ -10,7 +10,6 @@ User = get_user_model()
 
 class CreditPoint(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    # credit_points_earned = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
