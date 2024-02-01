96928870737256027e9c4ca07e1052acf01c0993
syntaxland <chibuzo.okenwa@email.com>
2024-02-01 13:48:21 +0100

    new update

--- credit_point/admin.py
+++ credit_point/admin.py
@@ -1,37 +1,37 @@
-from django.contrib import admin
-from . import models
+# from django.contrib import admin
+# from . import models
 
 
-@admin.register(models.CreditPointRequest)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'user', 'account_name', 'account_number', 
-                    'bank_name', 'credit_point_amount', 'request_ref', 'is_paid', 'paid_at',
-                    'is_delivered', 'delivered_at',)
-    search_fields = ('account_number', 'user')  
+# @admin.register(models.CreditPointRequest)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('created_at', 'user', 'account_name', 'account_number', 
+#                     'bank_name', 'credit_point_amount', 'request_ref', 'is_paid', 'paid_at',
+#                     'is_delivered', 'delivered_at',)
+#     search_fields = ('account_number', 'user')  
 
 
-@admin.register(models.CreditPoint)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 
-                    'user', 
-                    # 'credit_points_earned', 
-                    'balance', )
+# @admin.register(models.CreditPoint)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('created_at', 
+#                     'user', 
+#                     # 'credit_points_earned', 
+#                     'balance', )
 
-@admin.register(models.CreditPointPayment)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 
-                    'referrer', 
-                    'referral_credit_points_bonus',
-                     'order_payment', )
+# @admin.register(models.CreditPointPayment)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('created_at', 
+#                     'referrer', 
+#                     'referral_credit_points_bonus',
+#                      'order_payment', )
 
 
-@admin.register(models.CreditPointEarning)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('created_at',
-                     'user', 
-                    'credit_points_earned', 
-                    'order_payment',                  
-                      )
+# @admin.register(models.CreditPointEarning)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('created_at',
+#                      'user', 
+#                     'credit_points_earned', 
+#                     'order_payment',                  
+#                       )
     
 
 # @admin.register(models.BuyCreditPoint)
--- live_chat/admin.py
+++ live_chat/admin.py
@@ -1,27 +1,27 @@
-# live_chat/admin.py
-from django.contrib import admin
-from . import models
+# # live_chat/admin.py
+# from django.contrib import admin
+# from . import models
 
 
-@admin.register(models.ChatRoom)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('room_name', 'id', 'created_at', )  
-    search_fields = ('room_name',)
+# @admin.register(models.ChatRoom)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('room_name', 'id', 'created_at', )  
+#     search_fields = ('room_name',)
 
-    # def get_readonly_fields(self, request, obj=None):
-    #     readonly_fields = ['room_name', ]
-    #     if obj and obj.is_superuser:
-    #         readonly_fields.remove('name', )
-    #     return readonly_fields
+#     # def get_readonly_fields(self, request, obj=None):
+#     #     readonly_fields = ['room_name', ]
+#     #     if obj and obj.is_superuser:
+#     #         readonly_fields.remove('name', )
+#     #     return readonly_fields
 
 
-@admin.register(models.ChatMessage)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('room', 'user', 'message', 'timestamp',  )  
-    search_fields = ('name',)
+# @admin.register(models.ChatMessage)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('room', 'user', 'message', 'timestamp',  )  
+#     search_fields = ('name',)
 
-    # def get_readonly_fields(self, request, obj=None):
-    #     readonly_fields = ['room', 'user', 'message', ]
-    #     if obj and obj.is_superuser:
-    #         readonly_fields.remove('content', )
-    #     return readonly_fields
+#     # def get_readonly_fields(self, request, obj=None):
+#     #     readonly_fields = ['room', 'user', 'message', ]
+#     #     if obj and obj.is_superuser:
+#     #         readonly_fields.remove('content', )
+#     #     return readonly_fields
